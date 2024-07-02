8db057bcb1b5aad1ea7077e2d9fbc6196bd0f90d
Mirko Bozzetto <mirkouch@gmail.com>
2024-07-02 10:53:23 +0200

    fix(auth): correct type error in auth.ts by defining NextAuthConfig in NextAuth

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,61 +1,64 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { User } from '@prisma/client';
-import NextAuth, { Session } from 'next-auth';
-import { env } from '../env/server';
-import { prisma } from '../prisma';
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { User } from "@prisma/client";
+import NextAuth, { NextAuthConfig, Session } from "next-auth";
+import { env } from "../env/server";
+import { prisma } from "../prisma";
 import {
   credentialsOverrideJwt,
   credentialsSignInCallback,
-} from './credentialsProvider';
-import { getNextAuthConfigurationProviders } from './getNextAuthConfigurationProviders';
+} from "./credentialsProvider";
+import { getNextAuthConfigurationProviders } from "./getNextAuthConfigurationProviders";
 
 export const {
   handlers,
   signIn,
   signOut,
   auth: baseAuth,
-} = NextAuth(req => ({
-  pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    error: '/auth/error',
-    verifyRequest: '/auth/verify-request',
-    newUser: '/auth/new-user',
-  },
-  theme: {
-    logo: 'images/logo-text.png',
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: getNextAuthConfigurationProviders(),
-  session: {
-    strategy: 'database',
-  },
-  secret: env.NEXTAUTH_SECRET,
-  callbacks: {
-    session(params) {
-      if (params.newSession) return params.session;
+} = NextAuth(
+  (req) =>
+    ({
+      pages: {
+        signIn: "/auth/signin",
+        signOut: "/auth/signout",
+        error: "/auth/error",
+        verifyRequest: "/auth/verify-request",
+        newUser: "/auth/new-user",
+      },
+      theme: {
+        logo: "images/logo-text.png",
+      },
+      adapter: PrismaAdapter(prisma),
+      providers: getNextAuthConfigurationProviders(),
+      session: {
+        strategy: "database",
+      },
+      secret: env.NEXTAUTH_SECRET,
+      callbacks: {
+        session(params) {
+          if (params.newSession) return params.session;
 
-      const typedParams = params as unknown as {
-        session: Session;
-        user?: User;
-      };
+          const typedParams = params as unknown as {
+            session: Session;
+            user?: User;
+          };
 
-      if (!typedParams.user) return typedParams.session;
+          if (!typedParams.user) return typedParams.session;
 
-      typedParams.user.passwordHash = null;
+          typedParams.user.passwordHash = null;
 
-      return typedParams.session;
-    },
-  },
-  events: {
-    signIn: credentialsSignInCallback(req),
-    createUser: async message => {
-      const user = message.user;
+          return typedParams.session;
+        },
+      },
+      events: {
+        signIn: credentialsSignInCallback(req),
+        createUser: async (message) => {
+          const user = message.user;
 
-      if (!user.email) {
-        return;
-      }
-    },
-  },
-  jwt: credentialsOverrideJwt,
-}));
+          if (!user.email) {
+            return;
+          }
+        },
+      },
+      jwt: credentialsOverrideJwt,
+    } as NextAuthConfig)
+);
